7c29e408b8678a1167464b4fa0f4c013c4dca6b9
Karthikeyan C <114332111+cyber-karthi@users.noreply.github.com>
2024-06-21 14:34:41 +0530

    Update const.py

--- phpinfofilesleaks/utils/const.py
+++ phpinfofilesleaks/utils/const.py
@@ -12,7 +12,7 @@
 
 
 class Data:
-    blog = 'https://blogs.cappriciosec.com/blog/137/phpinfo-files-leaks'
+    blog = 'https://blogs.cappriciosec.com/blog/178/phpinfo-files-leaks'
     api = 'https://api.cappriciosec.com/Telegram/cappriciosecbot.php'
     config_path = '~/.config/cappriciosec-tools/cappriciosec.yaml'
     payloadurl = 'https://raw.githubusercontent.com/Cappricio-Securities/PayloadAllTheThings/main/phpinfo-files-leaks.txt'
